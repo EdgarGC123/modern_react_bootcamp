8bf55c407bf4a5a4f9f1b17726213b39f537cf2f
Edgar GC <Edgar.GalvanCuesta@gmail.com>
2021-02-09 18:24:25 -0600

    completed section 11, hangman

--- intro.js
+++ intro.js
@@ -13,4 +13,19 @@
 #16
 #22
 #28
+
+*/
+
+/*
+#68 talks about keyboard events all found on react site.
+ie
+onKeyUp - releasing a key after being pressed
+onKeyDown - when a key is pressed down
+onCopy - when ctrl c or right click copy is executed.
+onMouseEnter - when a mouse hovers over
+onMouseLeave - when a mouse hovers over then leaves.
+
+more can be found here:
+https://reactjs.org/docs/events.html#supported-events
+
 */
